79c44004b01e9faf643c0f0c4e3675b9fbcbcdbf
ffMathy <mathias.lorenzen@live.com>
2014-07-30 16:09:09 -0700

    added basic models.

---
+++ GitTipApi/GitTipApi/Models/ChartsItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GitTipApi.Models
+{
+    public class ChartsItem
+    {
+        public int ActiveUsers { get; set; }
+        public double Charges { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalGifts { get; set; }
+        public int TotalUsers { get; set; }
+        public double WeeklyGifts { get; set; }
+        public double Withdrawals { get; set; }
+    }
+}
---
+++ GitTipApi/GitTipApi/Models/ChartsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTipApi.Models
+{
+    public class ChartsResponse
+    {
+        public ChartsItem[] Items { get; set; }
+    }
+}
