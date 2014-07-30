7a886476d31f51fe751ac2f732f4a198454c2794
ffMathy <mathias.lorenzen@live.com>
2014-07-30 16:11:41 -0700

    added more models for stats.

---
+++ GitTipApi/GitTipApi/Models/StatsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTipApi.Models
+{
+    public class StatsResponse
+    {
+        public double AverageTip { get; set; }
+        public double AverageTippees { get; set; }
+        public double Escrow { get; set; }
+
+        public 
+    }
+}
---
+++ GitTipApi/GitTipApi/Models/StatsStatement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTipApi.Models
+{
+    public class StatsStatement
+    {
+        public string Statement { get; set; }
+
+        public string Username { get; set; }
+    }
+}
